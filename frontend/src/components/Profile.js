ca3b8829e680fa623de5bde9414aabdab1835695
Manan2505 <105840762+Manan2505@users.noreply.github.com>
2024-07-23 17:02:18 +0530

    Update Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -12,7 +12,7 @@ function Profile({ setIsLoggedIn }) {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get("http://localhost:8000/api/v3/users/user-details", {
+        const response = await axios.get("https://server-lake-kappa.vercel.app/api/v3/users/user-details", {
           withCredentials: true,
         });
         setUser(response.data.data);
